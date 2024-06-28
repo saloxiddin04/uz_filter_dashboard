8bf65e9be58e731a422f9534bd7d216b36df6ef8
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-06-28 16:38:41 +0500

    finish tabs

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -7,4 +7,5 @@ export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Loader } from './Loader';
+export { default as TabsRender } from './Tabs';
 
--- src/pages/contracts/ContractDetail.jsx
+++ src/pages/contracts/ContractDetail.jsx
@@ -1,27 +1,49 @@
-import React, {useEffect} from 'react';
-import {useParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {getContractDetail} from "../../redux/slices/contracts/contractsSlice";
-import {Loader} from "../../components";
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getContractDetail } from "../../redux/slices/contracts/contractsSlice";
+import { Loader, TabsRender } from "../../components";
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const tabs = [
+  {
+    title: 'Shartnoma',
+  },
+  {
+    title: "Shaxs ma'lumotlari",
+  },
+  {
+    title: "Ko'rib chiqish jarayoni",
+  },
+];
 
 const ContractDetail = () => {
-  const dispatch = useDispatch()
-  const {id} = useParams()
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { currentColor } = useStateContext();
+  const { contractDetail, loading } = useSelector(state => state.contracts);
 
-  const {contractDetail, loading} = useSelector(state => state.contracts)
+  const [openTab, setOpenTab] = useState(tabs.findIndex(tab => tab.active));
 
-  console.log(contractDetail)
+  console.log(openTab);
 
   useEffect(() => {
-    dispatch(getContractDetail(id))
+    dispatch(getContractDetail(id));
   }, [id]);
 
-  if (loading) return <Loader/>
+  if (loading) return <Loader />;
 
   return (
-    <div>
-      detail
-    </div>
+    <>
+      <div className="m-1 md:mx-4 md:my-10 mt-24 p-2 md:px-4 md:py-4 bg-white rounded-3xl">
+        <TabsRender
+          tabs={tabs}
+          color={currentColor}
+          openTab={openTab}
+          setOpenTab={setOpenTab}
+        />
+      </div>
+    </>
   );
 };
 
