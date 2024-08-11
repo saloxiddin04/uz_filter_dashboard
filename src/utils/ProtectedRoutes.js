42156aae6b3cfb859bcc6ed76f5caf7c7e430760
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-08-11 11:51:27 +0500

    oneid auth

--- src/utils/ProtectedRoutes.js
+++ src/utils/ProtectedRoutes.js
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux';
 const ProtectedRoutes = () => {
   const user = useSelector((state) => state.user.user);
   // const user = true;
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  // return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoutes;
