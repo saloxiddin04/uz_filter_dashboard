1a97d944f4abb89e61f472f92210e5ab518a75eb
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-08-11 12:39:35 +0500

    oneid auth

--- src/redux/slices/auth/Code.js
+++ src/redux/slices/auth/Code.js
@@ -7,7 +7,7 @@ import {
   oneIdLogin,
   setAccess,
   setAccessToken,
-  setCode,
+  setCode, setLogout,
   setRefresh, setUser
 } from "./authSlice";
 import {toast} from "react-toastify";
@@ -27,8 +27,14 @@ function Code() {
       if (res?.payload?.data) {
         await dispatch(setAccess(res.payload.data.access))
         await dispatch(setRefresh(res.payload.data.refresh))
-        let res2 = await dispatch(oneIdGetUserDetail(res?.payload?.data?.access)).then(() => navigate('/dashboard'))
+        let res2 = await dispatch(oneIdGetUserDetail(res?.payload?.data?.access))
         dispatch(setUser({payload: res2?.payload}))
+        if (res2?.payload?.userdata?.role?.name === 'Mijoz') {
+          toast.success('Muvaffaqiyatli avtorizatsiyadan otdingiz. Administrator tomonidan tizimga kirish uchun ruxsat berilishini kutishingizni soraymiz.')
+          dispatch(setLogout())
+        } else {
+          navigate('/dashboard')
+        }
       }
     } catch (e) {
       console.log(e)
