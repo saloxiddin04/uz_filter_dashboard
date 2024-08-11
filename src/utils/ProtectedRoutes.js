4ee5babec346aa56912b7c16e89342f20e5ad9e9
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-08-11 12:54:25 +0500

    oneid auth

--- src/utils/ProtectedRoutes.js
+++ src/utils/ProtectedRoutes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Outlet, Navigate, useLocation} from 'react-router-dom';
 import {useSelector} from 'react-redux';
 import Code from "../redux/slices/auth/Code";
@@ -6,6 +6,14 @@ import Code from "../redux/slices/auth/Code";
 const ProtectedRoutes = () => {
 	const {user} = useSelector((state) => state.user);
 	const {pathname} = useLocation();
+	
+	useEffect(() => {
+		if (pathname === '/code') {
+			return <Code/>;
+		} else {
+			return user ? <Outlet/> : <Navigate to="/login"/>;
+		}
+	}, [pathname]);
 	
 	if (pathname === '/code') {
 		return <Code/>;
