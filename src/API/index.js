728b04c52c897ef1c93662d6eb468076e48ad38b
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-08-16 15:48:37 +0500

    user-detail

--- src/API/index.js
+++ src/API/index.js
@@ -3,8 +3,7 @@ import {api_url} from '../config';
 import {toast} from "react-toastify";
 
 const access_token = localStorage.getItem("access") || "";
-const res = localStorage.getItem("res");
-const pin_or_tin = res ? JSON.parse(res) : [];
+const pin_or_tin = localStorage.getItem("tin_or_pin") || undefined;
 
 const instance = axios.create({
   baseURL: api_url,
