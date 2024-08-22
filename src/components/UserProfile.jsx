0b4d88920a32776dfd320146ee5d73ceb6c2fcfd
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-08-22 21:19:11 +0500

    change api url

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -66,7 +66,7 @@ const UserProfile = ({onClose}) => {
         </span>
         <div>
           <p className="font-semibold text-xl dark:text-gray-200"> {user?.full_name} </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400"> {user?.userdata?.role?.name} </p>
+          <p className="text-gray-500 text-sm dark:text-gray-400"> {user?.role} </p>
         </div>
       </div>
       <div>
