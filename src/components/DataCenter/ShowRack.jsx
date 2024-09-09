f42627ac4ee0dc3b99f20f567afa6bf9f60d5a04
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-09-09 11:28:02 +0500

    data_center reverse

--- src/components/DataCenter/ShowRack.jsx
+++ src/components/DataCenter/ShowRack.jsx
@@ -101,6 +101,8 @@ const ShowRack = () => {
     })
   }, [rack_detail, selectedUnits, drawer])
 
+  console.log(unitsData)
+
   let arr = selectedUnits?.filter((obj, index, self) => index === self.findIndex((t) => t.unit === obj.unit))
 
   const increment = () => {
@@ -133,7 +135,7 @@ const ShowRack = () => {
     if (deviceCount === 4) {
       return (
         <div
-          className="absolute top-1 left-5 w-[87%] h-[132px] ml-5 rounded cursor-pointer bg-cover border border-[#b6b6b6] bg-no-repeat z-10 text-white font-bold flex justify-center items-center"
+          className="absolute bottom-1 left-5 w-[87%] h-[132px] ml-5 rounded cursor-pointer bg-cover border border-[#b6b6b6] bg-no-repeat z-10 text-white font-bold flex justify-center items-center"
           onClick={() => showUnitInfo(number, busy, device)}
           style={{background: currentColor}}
         >
@@ -144,7 +146,7 @@ const ShowRack = () => {
     if (deviceCount === 3) {
       return (
         <div
-          className="absolute top-0 left-5 w-[87%] h-[100px] ml-5 rounded cursor-pointer bg-cover border border-[#b6b6b6] bg-no-repeat z-10 text-white font-bold flex justify-center items-center"
+          className="absolute bottom-0 left-5 w-[87%] h-[100px] ml-5 rounded cursor-pointer bg-cover border border-[#b6b6b6] bg-no-repeat z-10 text-white font-bold flex justify-center items-center"
           onClick={() => showUnitInfo(number, busy, device)}
           style={{background: currentColor}}
         >
@@ -155,7 +157,7 @@ const ShowRack = () => {
     if (deviceCount === 2) {
       return (
         <div
-          className="absolute top-0 left-5 w-[87%] h-[65px] ml-5 rounded cursor-pointer bg-cover border border-[#b6b6b6] bg-no-repeat z-10 text-white font-bold flex justify-center items-center"
+          className="absolute bottom-0 left-5 w-[87%] h-[65px] ml-5 rounded cursor-pointer bg-cover border border-[#b6b6b6] bg-no-repeat z-10 text-white font-bold flex justify-center items-center"
           onClick={() => showUnitInfo(number, busy, device)}
           style={{background: currentColor}}
         >
