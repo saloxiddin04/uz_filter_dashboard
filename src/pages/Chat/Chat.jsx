8efcdeeb5a20a6a5a989c328c2b0ddc421a8cd9c
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2024-11-22 12:24:32 +0500

    adding room_type

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -47,16 +47,16 @@ const Chat = () => {
 		}
 	}, [id]);
 	
-	useEffect(() => {
-		if (messagesEndRef.current) {
-			messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-		}
-	}, [messages]);
+	// useEffect(() => {
+	// 	if (messagesEndRef.current) {
+	// 		messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+	// 	}
+	// }, [messages]);
 	
 	const handleScroll = () => {
 		if (!modalBodyRef.current || loading || !hasMore) return;
-		
-		if (modalBodyRef.current.scrollTop === 0 && id) {
+
+		if (modalBodyRef.current.scrollTop === 0) {
 			loadMessages(page + 1);
 		}
 	};
@@ -100,7 +100,7 @@ const Chat = () => {
 		<div className="m-1 md:mx-4 md:my-10 mt-24 p-2 md:px-4 md:py-4 bg-white dark:bg-secondary-dark-bg rounded">
 			
 			<div className="flex h-[82vh] bg-gray-100 dark:bg-gray-900">
-				<div className="w-1/3 bg-white dark:bg-gray-800 shadow-md h-full overflow-y-auto">
+				<div className="w-[40%] bg-white dark:bg-gray-800 shadow-md h-full overflow-y-auto">
 					<div className="p-4 text-lg font-bold text-gray-800 dark:text-gray-200">Chats</div>
 					<div className="divide-y divide-gray-300 dark:divide-gray-700">
 						{loading ? (
@@ -109,16 +109,20 @@ const Chat = () => {
 							rooms?.map((room) => (
 								<div
 									key={room.id}
-									onClick={() => navigate(`/chat-messages/${slug}/${room.id}`)}
+									onClick={() => {
+										setPage(1)
+										setMaxPage(null)
+										setMessages([])
+										navigate(`/chat-messages/${slug}/${room.id}`)
+									}}
 									className={`p-4 cursor-pointer hover:bg-gray-200 dark:hover:bg-gray-700 flex justify-between items-center ${
 										id === room.id ? "bg-gray-200 dark:bg-gray-700" : ""
 									}`}
 								>
 									<div>
-										<p
-											className="text-gray-800 dark:text-gray-200">{room?.room_name?.length > 40 ? `${room?.room_name?.slice(0, 40)}...` : room?.room_name} </p>
-										<p className="text-sm text-gray-600 dark:text-gray-400">
-											{room?.last_message?.message?.length > 50 ? `${room?.last_message?.message?.slice(0, 50)}...` : room?.last_message?.message}
+										<p className="text-gray-800 dark:text-gray-200">{room?.room_name?.length > 40 ? `${room?.room_name?.slice(0, 40)}...` : room?.room_name} </p>
+										<p className="text-sm text-gray-600 dark:text-gray-400 pr-2">
+											{room?.last_message?.message?.length > 30 ? `${room?.last_message?.user?.name}: ${room?.last_message?.message?.slice(0, 30)}...` : `${room?.last_message?.user?.name}: ${room?.last_message?.message}`}
 										</p>
 									</div>
 									<p className="text-sm text-gray-600 dark:text-gray-400">{moment(room?.updated_time).format('DD-MM-YYYY HH:MM')}</p>
@@ -131,7 +135,7 @@ const Chat = () => {
 					<div className="flex-1 flex flex-col bg-gray-100 dark:bg-gray-900">
 						<div className="p-4 bg-white dark:bg-gray-800 shadow-md">
 							<h2 className="text-lg font-bold text-gray-800 dark:text-gray-200">
-								{rooms?.find((room) => room?.id === parseInt(id))?.room_name || "Chat"}
+								{rooms?.find((room) => room?.id === id)?.room_name || "Chat"}
 							</h2>
 						</div>
 						
@@ -152,7 +156,7 @@ const Chat = () => {
 												: "mr-auto bg-gray-200 text-black"
 										}`}
 									>
-										<p className="text-sm font-semibold">{msg.user.name}</p>
+										<p className={`text-sm font-semibold border-b-1 pb-1 ${!msg?.is_owner_client ? 'border-b-1' : 'border-b-1 border-b-blue-500'}`}>{msg.user.name}</p>
 										<p className="text-base">{msg.message}</p>
 										<p className="text-xs mt-2 text-right">
 											{moment(msg.updated_time).format("HH:mm")}
