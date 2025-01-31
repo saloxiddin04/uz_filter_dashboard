f256f085ff7cc697a9aaa8a04dd9d2b20dd8e744
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-01-31 08:33:16 +0500

    dashboard texts

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -7,12 +7,12 @@ const Dashboard = () => {
   const colors = ['#1A97F5', '#03C9D7', '#7352FF', '#1E4DB7', '#FB9678']
   
   const dataUserCount = [
-    {label: 'Распроданный', value: 20, color: colors[0]},
-    {label: 'Остальные', value: 50, color: colors[3]}
+    {label: 'Продано', value: 20, color: colors[0]},
+    {label: 'Осталось', value: 50, color: colors[3]}
   ];
   
   const dataProducts = [
-    {label: 'Готовый продукт', value: 1000, color: colors[2]},
+    {label: 'Типовой продукт', value: 1000, color: colors[2]},
     {label: 'На основании заказа', value: 50, color: colors[1]}
   ];
   
@@ -31,19 +31,19 @@ const Dashboard = () => {
         <div className="w-full flex justify-between flex-wrap mt-4 dark:text-white">
           <div className="card_dashboard w-full flex gap-4 justify-between">
             <div className="w-[25%] p-3 border rounded border-green-400 bg-green-500 text-white">
-              <p className="text-2xl">Доход</p>
+              <p className="text-2xl">Приход</p>
               <h1 className="text-2xl font-bold">+5000$</h1>
             </div>
             <div className="w-[25%] p-3 border rounded border-red-400 bg-red-500 text-white">
-              <p className="text-2xl">Выход</p>
+              <p className="text-2xl">Расход</p>
               <h1 className="text-2xl font-bold">-1000$</h1>
             </div>
             <div className="w-[25%] p-3 border rounded border-blue-400 bg-blue-500 text-white">
-              <p className="text-2xl">Всего продуктов</p>
+              <p className="text-2xl">Всего</p>
               <h1 className="text-2xl font-bold">700</h1>
             </div>
             <div className="w-[25%] p-3 border rounded border-yellow-400 bg-yellow-500 text-white">
-              <p className="text-2xl">Распроданный</p>
+              <p className="text-2xl">Продано</p>
               <h1 className="text-2xl font-bold">500</h1>
             </div>
           </div>
