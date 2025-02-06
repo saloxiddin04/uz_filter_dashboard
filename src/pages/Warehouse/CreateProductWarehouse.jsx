dcae1cb4b5552973e4b77433e7fc2fbfd1a7e1b0
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-02-06 14:36:54 +0500

    products for warehouse adding filter

--- src/pages/Warehouse/CreateProductWarehouse.jsx
+++ src/pages/Warehouse/CreateProductWarehouse.jsx
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Header, Pagination} from "../../components";
 import {useDispatch, useSelector} from "react-redux";
 import {useNavigate, useParams} from "react-router-dom";
 import {getProductsForWarehouse, getWarehouse} from "../../redux/slices/warehouse/warehouseSlice";
 import {useStateContext} from "../../contexts/ContextProvider";
 import Loader from "../../components/Loader";
-import {EyeIcon, PencilIcon} from "@heroicons/react/16/solid";
+import {ArrowPathIcon, EyeIcon, FunnelIcon, PencilIcon} from "@heroicons/react/16/solid";
+import {getAllCategories} from "../../redux/slices/utils/category/categorySlice";
+import {Debounce} from "../../utils/Debounce";
 
 const CreateProductWarehouse = () => {
   const dispatch = useDispatch()
@@ -14,6 +16,7 @@ const CreateProductWarehouse = () => {
   const {id} = useParams()
   
   const {loading, warehouse, productsForWarehouse} = useSelector(state => state.warehouse)
+  const {categories} = useSelector(state => state.category)
   
   const [handleFilter, setFilter] = useState(false)
   const [unique_code, setUniqueCode] = useState(undefined)
@@ -26,7 +29,25 @@ const CreateProductWarehouse = () => {
       filters: {page: 1, page_size: 10},
     }))
     dispatch(getWarehouse(id))
+    dispatch(getAllCategories())
   }, [id, dispatch])
+  
+  const debouncedSearch = useCallback(
+    Debounce((filters) => {
+      dispatch(getProductsForWarehouse({ filters, warehouse_id: id }));
+    }, 500),
+    [dispatch, id]
+  )
+  
+  useEffect(() => {
+    if (handleFilter) {
+      debouncedSearch({
+        product_name,
+        unique_code,
+        category,
+      });
+    }
+  }, [product_name, unique_code, category, debouncedSearch]);
   
   const handleChangePage = (page) => {
     dispatch(getProductsForWarehouse({
@@ -35,18 +56,93 @@ const CreateProductWarehouse = () => {
     }))
   }
   
-  
   return (
     <div className="card">
       <div className={'flex items-start justify-between mb-4'}>
         <Header category="Страница" title={warehouse?.name} />
-        <button
-          className={'px-4 py-2 rounded text-white'}
-          style={{backgroundColor: currentColor}}
-          onClick={() => navigate('addProduct')}
-        >
-          Добавить товар
-        </button>
+        
+        {handleFilter && (
+          <>
+            <div className="flex justify-center gap-4 items-center w-[65%]">
+              <div className={'flex flex-col w-[35%]'}>
+								<label className="block text-gray-700 text-sm font-bold mb-1 ml-3" htmlFor="amount">
+									Название продукта
+								</label>
+								<input
+                  value={product_name || ""}
+                  onChange={(e) => setProductName(e.target.value)}
+                  name="amount"
+                  id="amount"
+                  type="text"
+                  className="rounded w-full py-1.5 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow focus:border-blue-500 border mb-1"
+                />
+							</div>
+              <div className={'flex flex-col w-[35%]'}>
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-1 ml-3"
+                  htmlFor="mounting_type">Категория</label>
+                <select
+                  className={'w-full px-1 py-1 rounded focus:outline-none focus:shadow focus:border-blue-500 border mb-1'}
+                  value={category || ''}
+                  onChange={(e) => setCategory(e.target.value)}
+                  name="mounting_type"
+                  id="mounting_type"
+                >
+                  <option value={undefined} disabled={category}>Tanlang</option>
+                  {categories?.map((item) => (
+                    <option value={item?.id} key={item?.id}>{item?.name}</option>
+                  ))}
+                </select>
+              </div>
+              <div className={'flex flex-col w-[35%]'}>
+								<label className="block text-gray-700 text-sm font-bold mb-1 ml-3" htmlFor="amount">
+									Код продукта
+								</label>
+								<input
+                  value={unique_code || ""}
+                  onChange={(e) => setUniqueCode(e.target.value)}
+                  name="amount"
+                  id="amount"
+                  type="text"
+                  className="rounded w-full py-1.5 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow focus:border-blue-500 border mb-1"
+                />
+							</div>
+            </div>
+          </>
+        )}
+        
+        <div className="flex items-center gap-6 pt-5">
+          {handleFilter ? (
+            <button
+              className={`px-2 py-1 rounded border text-center`}
+              style={{borderColor: currentColor}}
+              onClick={() => {
+                setFilter(false)
+                setProductName(undefined)
+                setUniqueCode(undefined)
+                setCategory(undefined)
+                dispatch(getProductsForWarehouse({
+                  warehouse_id: id,
+                  filters: {page: 1, page_size: 10},
+                }))
+              }}
+            >
+              <ArrowPathIcon className="size-6" fill={currentColor} />
+            </button>
+          ) : (
+            <button title="filter" onClick={() => setFilter(true)}>
+              <FunnelIcon className="size-6" color={currentColor} />
+            </button>
+          )}
+          
+          <button
+            className={'px-4 py-2 rounded text-white'}
+            style={{backgroundColor: currentColor}}
+            onClick={() => navigate('addProduct')}
+          >
+            Добавить товар
+          </button>
+        </div>
       </div>
       
       <div className="relative overflow-x-auto shadow-md sm:rounded">
---
+++ src/utils/Debounce.js
@@ -0,0 +1,7 @@
+export const Debounce = (func, delay) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay);
+  };
+};
